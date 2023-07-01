2003bf510782fc4098d5eed1b4f032d57a94ee01
Jose Berrio <aldarisbm@gmail.com>
2023-07-01 00:12:13 -0400

    is getting late

--- main.py
+++ main.py
@@ -6,7 +6,8 @@ from langchain.llms import OpenAI
 def main():
     load_dotenv()
     llm = OpenAI(openai_api_key=os.environ.get("OPENAI_API_KEY"), temperature=0.5)
-    llm.predict("What is the best dog breed?")
+    prediction = llm.predict("What is the best dog breed?")
+    print(prediction)
 
 
 # Press the green button in the gutter to run the script.
