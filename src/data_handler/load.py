461bd5cd2356497e0acb2948bff7d86a7cee9030
Jose Berrio <aldarisbm@gmail.com>
2023-10-04 23:12:59 -0400

    chroma taking foreva

--- src/data_handler/load.py
+++ src/data_handler/load.py
@@ -1,5 +1,7 @@
 from typing import List
 from langchain.vectorstores import VectorStore
 
-def load_data(index: str, passages: List[str], vectorStore: VectorStore):\
-    pass
+def load_data(index: str, passages: List[str], vector_store: VectorStore):
+    vector_store.add_texts(passages)
+    print('loaded vectors')
+    return
--- src/main.py
+++ src/main.py
@@ -4,6 +4,7 @@ from dotenv import load_dotenv
 
 from src.helpers.state_handler import write_to_json
 from src.data_handler.read import read_data
+from src.data_handler.load import load_data
 from langchain.vectorstores import Chroma
 from langchain.embeddings import SentenceTransformerEmbeddings
 
@@ -17,27 +18,30 @@ def main():
     print(f"Processing file: {data_loc}...")
 
     if not os.path.exists(local_map_loc):
-        # if json doesn't exist, let's create it
-        # this should probably an index db
+        # if json doesn't exist, let's create an empty object
+        # this should probably an index db, but its okay for local dev
         with open(local_map_loc, 'w+') as f:
             json.dump({}, f)
 
-    with open(local_map_loc, ) as f:
+    with open(local_map_loc) as f:
         basename = os.path.basename(data_loc)
         dl_key = basename.lower().replace('.', '_').replace(' ', '_').replace('-', '_')
-        print(f"Saving key: {dl_key} with value {data_loc}")
-
+        print(f"Saving key: {dl_key} with value: {data_loc}")
         local_map = json.load(f)
+
         print('Checking if file has already been processed...')
         if dl_key in local_map:
-            print('Data has already been loaded.')
+            print('Data has already been loaded...')
         else:
             print('Starting data processing...')
+            chroma_db = Chroma(persist_directory=os.getenv("PERSIST_DIRECTORY"), embedding_function=SentenceTransformerEmbeddings())
             data = read_data(data_loc)
+            load_data(dl_key, data, chroma_db)
+
             local_map[dl_key] = data_loc
+            print(f'saving: {local_map}')
             json.dump(local_map, f)
 
-    # chroma_db = Chroma.from_texts(texts=data, embeddings=SentenceTransformerEmbeddings())
     # print(len(data))
 
 
